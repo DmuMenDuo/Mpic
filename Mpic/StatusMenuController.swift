f47ab077c23fdfc59cc985ce4e90955062634834
menduo <demon9607@163.com>
2018-11-15 00:54:45 +0800

    我又回来啦

--- Mpic/StatusMenuController.swift
+++ Mpic/StatusMenuController.swift
@@ -9,7 +9,18 @@
 import Cocoa
 
 class StatusMenuController: NSObject {
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    override func awakeFromNib() {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        statusItem.button?.image = icon
+        statusItem.menu = statusMenu
+    }
+    
 }
- 
- 
